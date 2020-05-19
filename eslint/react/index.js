e54ff3077a581652a9c44c8dddd74430c6ef35af
Alex LaFroscia <alex@lafroscia.com>
2020-05-19 11:47:29 -0400

    feat: upgrade base React config

--- eslint/react/index.js
+++ eslint/react/index.js
@@ -1,5 +1,28 @@
 'use strict';
 
 module.exports = {
-  extends: ['plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ['plugin:react/recommended', 'prettier/react'],
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/tests/**/*.{js,jsx,ts,tsx}'],
+      rules: {
+        'react/jsx-props-no-spreading': 'off',
+      },
+    },
+  ],
 };
