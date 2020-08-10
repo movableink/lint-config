54bb7b59f61f2a038b8e71b7109434ac39198d2b
Alex LaFroscia <alex@lafroscia.com>
2020-08-10 10:44:32 -0400

    test: increase timeout during dep install

--- generator-lint-config/__tests__/app/workspace-root.js
+++ generator-lint-config/__tests__/app/workspace-root.js
@@ -5,6 +5,8 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 const fs = require('fs-extra');
 
+const TIMEOUT_MS = 10000;
+
 beforeAll(() => {
   return helpers
     .run(path.join(__dirname, '../../generators/app'))
@@ -13,9 +15,14 @@ beforeAll(() => {
       fs.copy(path.join(__dirname, '../fixtures/mono-repo'), dir, done);
     })
     .withOptions({ 'skip-install': false });
+}, TIMEOUT_MS);
+
+test('it installs the ESLint config', () => {
+  assert.fileContent('package.json', '@movable/eslint-config');
+  assert.fileContent('package.json', '@movable/prettier-config');
 });
 
-test('it runs the ESLint generator', () => {
+test('it installs the Prettier config', () => {
   assert.fileContent('package.json', '@movable/eslint-config');
   assert.fileContent('package.json', '@movable/prettier-config');
 });
